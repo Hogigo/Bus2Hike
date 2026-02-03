0d3fd61b73ceab4810430b088e1a06a3acc992b7
Hogigo <ondra_muller@protonmail.com>
2026-02-03 15:00:01 +0100

    Updated endpoint for Antonia<3

--- backend/app/api/transport_stops/crud.py
+++ backend/app/api/transport_stops/crud.py
@@ -1,7 +1,9 @@
+from typing import List
+
 from psycopg2.extras import RealDictCursor
 
 from app.db import engine
-from .schemas import TransportStopGetDto
+from .schemas import TransportStopGetDto, FilterParams
 
 
 def get_all_transport_stops():
@@ -14,15 +16,47 @@ def get_all_transport_stops():
         FROM transport_stops
         ORDER BY id
     """
+    dtos = execute_query_and_return_list_of_dtos(LIST_TRANSPORT_STOPS_SQL)
+    return dtos
+
+def get_transport_stop_by_filter_params(filter_params: FilterParams) -> List[TransportStopGetDto]:
+    params = (
+        filter_params.longitude,
+        filter_params.latitude,
+        filter_params.range_km,
+    )
+    dtos = execute_query_and_return_list_of_dtos(LIST_ALL_TRANSPORT_STOPS_NEAR_POINT, params)
+    print(len(dtos))
+    return dtos
+
+
+def execute_query_and_return_list_of_dtos(query: str, params: tuple = None):
     with engine.connect() as connection:
         raw_conn = connection.connection
         try:
             with raw_conn.cursor(cursor_factory=RealDictCursor) as cursor:
-                cursor.execute(LIST_TRANSPORT_STOPS_SQL)
+                cursor.execute(query, params)
                 rows = cursor.fetchall()
-            dtos = [TransportStopGetDto.from_row(r) for r in rows]
+
+            dtos = [TransportStopGetDto.from_row(dict(r)) for r in rows]
             return dtos
         except Exception as e:
             raw_conn.rollback()
             raise RuntimeError("Failed to fetch from database: " + str(e))
 
+
+LIST_ALL_TRANSPORT_STOPS_NEAR_POINT="""
+SELECT
+    id,
+    name,
+    created_at,
+    ST_AsGeoJSON(geometry)::json AS geometry
+FROM transport_stops
+WHERE ST_DWithin(
+    geometry::geography,
+    ST_SetSRID(ST_MakePoint(%s, %s), 4326)::geography,
+    %s * 1000
+)
+ORDER BY id;
+
+"""
--- backend/app/api/transport_stops/schemas.py
+++ backend/app/api/transport_stops/schemas.py
@@ -1,6 +1,8 @@
+from fastapi import APIRouter, Query, Depends, HTTPException
 from pydantic import BaseModel, ConfigDict
 from datetime import datetime
 from typing import Any
+from typing import Optional, List
 
 GeoJSON = dict[str, Any]
 
@@ -16,3 +18,17 @@ class TransportStopGetDto(BaseModel):
     @classmethod
     def from_row(cls, row: dict) -> "TransportStopGetDto":
         return cls.model_validate(row)
+
+
+class FilterParams(BaseModel):
+    longitude: Optional[float] = Query(default=11.33982, ge=-180, le=180)
+    latitude: Optional[float] = Query(default=46.49067, ge=-90, le=90)
+    range_km: Optional[float] = Query(default=100, gt=0)
+
+    def all_query_params_missing(self) -> bool:
+        all_values = self.model_dump().values()
+        return all(value is None for value in all_values)
+
+    def all_query_params_present(self) -> bool:
+        all_values = self.model_dump().values()
+        return all(v is not None for v in all_values)
