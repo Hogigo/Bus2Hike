2c4d61dca264033f4164fb22131112874bd053b7
Hogigo <ondra_muller@protonmail.com>
2026-02-03 18:00:26 +0100

    Added description generated by llm

--- backend/app/api/hikes/routes.py
+++ backend/app/api/hikes/routes.py
@@ -3,7 +3,8 @@ from app.find_trails import find_trails
 import json
 
 from .schemas import TrailFilterParams
-from app.generate_ai_description import generate_description
+from app.generate_ai_description import generate_and_add_description
+
 router = APIRouter(prefix="/hikes", tags=["hikes"])
 
 
@@ -15,8 +16,9 @@ def list_hikes(filter_params: TrailFilterParams = Depends()):
                          filter_params.diameter,
                          filter_params.max_distance,
                          filter_params.max_paths)
-    generate_description(trails)
-    return json.loads(trails)
+    if trails:
+        trails_with_descriptions = generate_and_add_description(trails)
+    return json.loads(trails_with_descriptions)
 
 
 
